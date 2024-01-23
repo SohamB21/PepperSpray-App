8a7ec97c2c45bdc6f3a7a0e96ebb6efb70352b7d
Sharmili Das <128738858+sharmilidas33@users.noreply.github.com>
2024-01-23 13:27:07 +0530

    Update ProfileScreen.tsx
    Updating Profile Screen

--- app/screens/ProfileScreen.tsx
+++ app/screens/ProfileScreen.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {View, Text, ScrollView, StyleSheet} from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import Header from '../components/Header';
-import {COLORS} from '../theme/theme';
+import ProfileAbout, { UserProfile } from '../components/ProfileAbout';
+import { COLORS } from '../theme/theme';
+import AchievementBadges from '../components/AchievementBadges';
+import GoldAward from '../assets/badges/GoldAward.png';
+import SilverAward from '../assets/badges/SilverAward.png';
+import BronzeAward from '../assets/badges/BronzeAward.png';
 
 const ProfileScreen = () => {
+  const userProfile: UserProfile = {
+    userName: "Zendaya",
+    userId: "zendaya10",
+    userBio: "Zendaya Maree Stoermer Coleman is an American actress and singer.",
+    location: "California, US",
+    joinedMonth: "May",
+    joinedYear: "2023",
+    verified: true,
+    profilePicture: "https://i.pinimg.com/originals/24/ca/51/24ca51edd82d5828340f6a87edbbb529.jpg",
+  };
+
+  const achievementsData = [
+    { title: "Safety Advocate", icon: GoldAward },
+    { title: "Bug Buster", icon: SilverAward },
+    { title: "Consistent Connector", icon: BronzeAward },
+    { title: "Spread the Safety", icon: GoldAward },
+    { title: "Verified Pepper", icon: BronzeAward },
+  ];
+
   return (
     <View style={styles.container}>
       <Header screen="Profile" icon="settings" />
       <ScrollView contentContainerStyle={styles.contentContainer}>
-        <Text style={styles.welcomeText}>Welcome to Profile Sharmili!</Text>
+        <ProfileAbout user={userProfile} />
+        <AchievementBadges achievementsData={achievementsData}/>
       </ScrollView>
     </View>
   );
@@ -20,7 +45,7 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.Background,
   },
   contentContainer: {
-    padding: 16,
+    padding: 12,
   },
   welcomeText: {
     color: COLORS.PrimaryText,
@@ -28,4 +53,5 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
 });
+
 export default ProfileScreen;
